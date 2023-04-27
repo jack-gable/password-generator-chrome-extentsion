ab7ade924cfacafa8a22a0b28f2d0a26eb2a12a5
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2023-04-27 15:58:24 -0700

    initial files, rearranged tooltip

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+];
+
+const generatorBtn = document.querySelector("#generator-btn");
+let output = document.querySelector("#pw");
+const clipboardIcon = document.querySelector(".fa-clipboard");
+const tooltipText = document.querySelector(".tooltiptext");
+
+generatorBtn.addEventListener("click", () => {
+	output.textContent = "";
+	for (let i = 0; i < 16; i++) {
+		let chars = Math.floor(Math.random() * characters.length);
+		output.textContent += characters[chars];
+	}
+	tooltipText.textContent = "Copy to clipboard";
+});
+
+clipboardIcon.addEventListener("click", () => {
+	navigator.clipboard.writeText(output.textContent);
+	tooltipText.textContent = "Copied!";
+});
